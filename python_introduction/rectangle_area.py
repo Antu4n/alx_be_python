a8e6f7d5708833fc64e3d772bb3c5abd4ba6fb18
Antu4n <githinji.antony.agm@gmail.com>
2025-07-13 16:20:30 +0300

    Initial commit

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+#area of a triangle
+length = 10
+width = 5
+area = width * length
+print(f"The area of the rectangle is: {area}")
+
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+#simple interest calculator
+p = 1000
+rate = 0.05
+time = 3
+interest = (p*rate*time)
+print(f"The simple interest is: {interest}")
+
