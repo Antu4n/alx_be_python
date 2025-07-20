1c17f3c1ddf57386fa72c25eb0521b85a35fc611
Antu4n <githinji.antony.agm@gmail.com>
2025-07-20 18:12:56 +0300

    Add control_flow directory

---
+++ control_flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+#calculator with match case
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+
+match operator:
+	case "+":
+		print(f'The result is {num1+num2}.')
+	case "-":
+		print(f"The result is {num1-num2}.")
+	case "*":
+		print(f"The result is {num1*num2}.")
+	case "/":
+		print(f"The result is {num1/num2}.")
+
---
+++ control_flow/pattern_drawing.py
@@ -0,0 +1,8 @@
+pattern_size = int(input("Enter the size of the pattern: "))
+for _ in range(pattern_size):
+    for _ in range(pattern_size):
+        print("*", end="")
+    print("\n")
+    	
+
+
