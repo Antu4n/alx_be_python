147d222bb3fa2df1478457a13dc16f822b5fe997
Antu4n <githinji.antony.agm@gmail.com>
2025-07-24 18:12:26 +0300

    Correction

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,15 +1,16 @@
 #datetime module
-import datetime
+from datetime import datetime
+from datetime import timedelta
 def display_current_datetime():
-	current_date = datetime.datetime.now()
+	current_date = datetime.now()
 	current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
 	print(f"Current date and time: {current_date}")
 display_current_datetime()
 
 days = int(input("Enter number of days to add to the current date: "))
 def calculate_future_date(days):
-	current_date = datetime.datetime.now()
-	future_date = (current_date + datetime.timedelta(days = days))
+	current_date = datetime.now()
+	future_date = (current_date + timedelta(days = days))
 	future_date = future_date.strftime("%Y-%m-%d")
 	print(f"Future date: {future_date}")
 
--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,26 +1,29 @@
-#temp conversion
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 def convert_to_celsius(fahrenheit):
-	celsius = ((fahrenheit-32) * FAHRENHEIT_TO_CELSIUS_FACTOR)
-	print(f"{fahrenheit}°F is {round(celsius, 1)}°C")
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit}°F is {round(celsius, 1)}°C")
+
 def convert_to_fahrenheit(celsius):
-	fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR)+32
-	print(f"{celsius}°C is {round(fahrenheit,1)}°F")
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{celsius}°C is {round(fahrenheit, 1)}°F")
 
 while True:
-	temperature = float(input("Enter the temperature to convert: "))
-	if isinstance(temperature, float) == False:
-		float(input("Please enter a numeric value: "))
-		continue
-	degree = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
-	if degree == "c":
-		convert_to_fahrenheit(temperature)
-	elif degree == "f":
-		convert_to_celsius(temperature)
-	else: 
-		print("Enter Valid input") 
-		continue
+    try:
+        temperature = float(input("Enter the temperature to convert: "))
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
+        continue
 
-	break
+    degree = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
+
+    if degree == "c":
+        convert_to_fahrenheit(temperature)
+    elif degree == "f":
+        convert_to_celsius(temperature)
+    else:
+        print("Enter valid input (C/F).")
+        continue
+
+    break
